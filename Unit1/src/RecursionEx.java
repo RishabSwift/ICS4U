8b037583f9c65ce24db5cd49e927a92b75a23fc2
Rishab Bhatt <rishabswift@gmail.com>
2018-02-15 14:37:04 -0500

    Initial commit

---
+++ Unit1/src/RecursionEx.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class RecursionEx {
+
+	public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws Exception {
+
+		char letter = ' ';
+		System.out.println("Type the characters HELLO* at the -> ");
+		getLetter1(letter);
+
+		System.out.println("Type the characters WOW* at the -> ");
+		getLetter2();
+	}
+
+	public static void getLetter1(char c) throws Exception {
+		c = (char) in.read();
+		if (c != '*') {
+			getLetter1(c);
+		}
+
+		System.out.println(c);
+	}
+
+	public static void getLetter2() throws Exception {
+		char c = (char) in.read();
+		if (c != '*') {
+			getLetter1(c);
+		}
+
+		System.out.println(c);
+	}
+}
