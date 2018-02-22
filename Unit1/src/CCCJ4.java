87bdc4071396c7c152a9222849577652081357d2
Rishab Bhatt <rishabswift@gmail.com>
2018-02-22 14:09:49 -0500

    Added debug mode, comments and optimized it

--- Unit1/src/CCCJ4.java
+++ Unit1/src/CCCJ4.java
@@ -5,6 +5,8 @@ import java.util.Stack;
 
 public class CCCJ4 {
 
+	public static final boolean DEBUG = false;
+
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
@@ -15,7 +17,7 @@ public class CCCJ4 {
 			// Check if they have entered letters
 			if (isValidInput(line)) {
 
-				convertToPostfix(line);
+				System.out.println(convertToPostfix(line));
 
 				line = scan.nextLine();
 
@@ -27,11 +29,59 @@ public class CCCJ4 {
 
 		}
 
-		System.out.println("Program Finished");
-		
-	
+		if (DEBUG)
+			System.out.println("Program Finished");
+
 	}
-	
+
+	/**
+	 * Convert prefix to postfix
+	 * 
+	 * @param line
+	 */
+	public static String convertToPostfix(String line) {
+
+		// get the prefix in the array so each character is an element in the array
+		String[] prefix = line.split(" ");
+
+		// Store the final postfix in stack so we can pop and pus!
+		Stack<String> postfix = new Stack<String>();
+
+		String operand1, operand2;
+
+		// Loop through the prefix array backwards and start checking if there is an
+		// operator
+		for (int i = prefix.length - 1; i >= 0; i--) {
+
+			// If it's not an operator... push the element into the stack
+			if (Character.isDigit(prefix[i].charAt(0))) {
+				String string = postfix.push(prefix[i]);
+
+				if (DEBUG)
+					System.out.println("Just popped " + string);
+
+				// It seems like the character is a operator
+			} else {
+
+				// Take the last two and add the operator at the end
+				operand1 = postfix.pop();
+				operand2 = postfix.pop();
+
+				if (DEBUG)
+					System.out.println(" Just popped operator 1 and 2 >> " + operand1 + " & " + operand2);
+
+				// Parse it together
+				postfix.push(operand1 + " " + operand2 + " " + prefix[i]);
+
+				if (DEBUG)
+					System.out.println("Just pushed " + (operand1 + " " + operand2 + " " + prefix[i]));
+			}
+
+		}
+
+		// The stack will now have the final answer... we need to get that and return it
+		return postfix.pop();
+	}
 
 	/**
 	 * If they have entered a valid input i.e. no characters
@@ -47,55 +97,5 @@ public class CCCJ4 {
 		}
 		return true;
 	}
-
-	public static void convertToPostfix(String line) {
-		
-		ArrayList<String> finalAnswer = new ArrayList<String>(); 
-		
-		// get the prefix in the array so each character is an element in the array
-		String[] prefix = line.split(" ");
-		
-		// Store the final postfix in stack so we can pop and pus!
-		Stack<String> postfix = new Stack<String>();
-		
-		String operand1, operand2;
-
-	
-		
-		// Loop through the prefix array backwards and start checking if there is an operator
-		for (int i = prefix.length - 1; i >= 0; i--) {
-
-			// If it's not an operator... push the element into the stack
-			if (Character.isDigit(prefix[i].charAt(0))) {
-				String string = postfix.push(prefix[i]);
-				System.out.println("Just popped " + string);
-				
-				// It seems like the character is a operator
-			} else {
-				
-				// Take the last two and add the operator at the end
-				operand1 = postfix.pop();
-				operand2 = postfix.pop();
-				
-				System.out.println(" Just popped operator 1 and 2 >> " + operand1 + " & " + operand2);
-				
-				// Parse it together
-				postfix.push(operand1 + " " + operand2 + " " + prefix[i]);
-				
-				
-				System.out.println("Just pushed " + (operand1 + " " + operand2 + " " + prefix[i]));
-			}
-			
-		}
-		
-		finalAnswer.add(postfix.pop());
-		
-		for (String s : finalAnswer ) {
-			System.out.println(s);
-		}
-		
-		
-	}
-	
 
 }
